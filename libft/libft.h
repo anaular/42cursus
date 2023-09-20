6c7d780aade660cd1ab2f946456734cddac84683
anaular <anaular@student.42madrid.com>
2023-09-20 19:48:08 +0200

    agregado ft_tolower.c y ft_toupper.c

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: anaular <anaular@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:02:56 by anaular           #+#    #+#             */
-/*   Updated: 2023/09/20 19:17:49 by anaular          ###   ########.fr       */
+/*   Updated: 2023/09/20 19:47:07 by anaular          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ int			ft_isdigit(int c);
 int			ft_isalnum(int c);
 int			ft_isascii(int c);
 int			ft_isprint(int c);
-
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 void		*ft_memset(void *b, int c, size_t len);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		ft_bzero(void *s, size_t n);
