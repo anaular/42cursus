395a939e9b17004c43d8e601875662c2237feb08
anaular <anaular@student.42madrid.com>
2023-09-22 12:19:39 +0200

    ft_atoi agregado

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: anaular <anaular@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 18:02:56 by anaular           #+#    #+#             */
-/*   Updated: 2023/09/22 11:15:22 by anaular          ###   ########.fr       */
+/*   Updated: 2023/09/22 12:18:08 by anaular          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int			ft_isascii(int c);
 int			ft_isprint(int c);
 int			ft_toupper(int c);
 int			ft_tolower(int c);
+int			ft_atoi(const char *str);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memset(void *b, int c, size_t len);
