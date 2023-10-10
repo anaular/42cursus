dd0484b5f6d61e9ff2ae6308fc6347cac6201319
Angelo Aular <88501499+anaular@users.noreply.github.com>
2023-10-10 20:41:42 +0200

    Add files via upload

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anaular <anaular@student.42madrid.>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 19:48:44 by anaular           #+#    #+#             */
+/*   Updated: 2023/10/10 20:39:22 by anaular          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	str = ft_strdup(s);
+	if (!s || !f || !str)
+		return (0);
+	i = 0;
+	while (str[i])
+	{
+		str[i] = f(i, str[i]);
+		i++;
+	}
+	return (str);
+}
