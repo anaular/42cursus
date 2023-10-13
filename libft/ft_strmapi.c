ded53bdb0136fca1860319af2d81c0e958679aff
anaular <anaular@student.42madrid.com>
2023-10-13 19:33:23 +0200

    act

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -3,33 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-<<<<<<< HEAD
 /*   By: anaular <anaular@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/13 19:11:18 by anaular           #+#    #+#             */
-/*   Updated: 2023/10/13 19:23:09 by anaular          ###   ########.fr       */
-=======
-/*   By: anaular <anaular@student.42madrid.>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 19:48:44 by anaular           #+#    #+#             */
-/*   Updated: 2023/10/10 20:39:22 by anaular          ###   ########.fr       */
->>>>>>> dd0484b5f6d61e9ff2ae6308fc6347cac6201319
+/*   Created: 2023/10/13 19:31:56 by anaular           #+#    #+#             */
+/*   Updated: 2023/10/13 19:32:51 by anaular          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-<<<<<<< HEAD
-char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*res;
 
 	if (!s || !f)
-		return (0);
+		return (NULL);
 	res = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
 	if (!res)
-		return (0);
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
@@ -38,21 +30,4 @@ char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 	}
 	res[i] = '\0';
 	return (res);
-=======
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
-{
-	char			*str;
-	unsigned int	i;
-
-	str = ft_strdup(s);
-	if (!s || !f || !str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		str[i] = f(i, str[i]);
-		i++;
-	}
-	return (str);
->>>>>>> dd0484b5f6d61e9ff2ae6308fc6347cac6201319
 }
